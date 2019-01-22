5d8a6f4b035b7cff6e462c541ebcaff00b70c945
edfeld <edfeld1@gmail.com>
2019-01-22 12:08:07 -0800

    a couple of cosmetic changes I missed saving

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -32,31 +32,6 @@ const strategy = new LocalStrategy(
 			return done(err)
 		});
 		
-			// , (err, userMatch) => {
-			// 	if (err) {
-			// 		return done(err)
-			// 	}
-			// 	if (!userMatch) {
-			// 		return done(null, false, { message: 'Incorrect username' })
-			// 	}
-			// 	if (!userMatch.checkPassword(password)) {
-			// 		return done(null, false, { message: 'Incorrect password' })
-			// 	}
-			// 	return done(null, userMatch)
-			// }
-		// )
-		// User.findOne({ 'local.username': username }, (err, userMatch) => {
-		// 	if (err) {
-		// 		return done(err)
-		// 	}
-		// 	if (!userMatch) {
-		// 		return done(null, false, { message: 'Incorrect username' })
-		// 	}
-		// 	if (!userMatch.checkPassword(password)) {
-		// 		return done(null, false, { message: 'Incorrect password' })
-		// 	}
-		// 	return done(null, userMatch)
-		// })
 	}
 )
 
