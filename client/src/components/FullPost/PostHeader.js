f2f8fab42a021d9e000dcef6c67bb8dda5829d92
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-01-30 10:49:44 -0800

    reverted all colors and images

--- client/src/components/FullPost/PostHeader.js
+++ client/src/components/FullPost/PostHeader.js
@@ -5,7 +5,7 @@ const PostHeader = props => {
     console.log('logged inside of postHeader ', props.post.post.title)
     return(
    <div style={{position: 'sticky', top: '.1px', zIndex: '500'}}>
-       <div style={{borderRadius: '0px', width:'100%', background: '#d59181',height:'auto',justifyContent: 'center'}} class="jumbotron jumbotron-fluid">
+       <div style={{borderRadius: '0px', width:'100%', backgroundImage: 'url(https://images.pexels.com/photos/1093909/pexels-photo-1093909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',height:'auto',justifyContent: 'center'}} class="jumbotron jumbotron-fluid">
             <div class="container">
                 <h1 class="display-4">{props.post.post.title}</h1>
                 <p class="lead">{props.post.post.context}</p>
