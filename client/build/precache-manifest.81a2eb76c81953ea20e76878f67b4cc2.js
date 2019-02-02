e943e85eb3757c08d5e9e4aa8bb3899c872428f2
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-02-02 10:56:14 -0800

    merging master

---
+++ client/build/precache-manifest.81a2eb76c81953ea20e76878f67b4cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0a941f552902f9a3acd",
+    "url": "/static/js/main.f0a941f5.chunk.js"
+  },
+  {
+    "revision": "1e7806449dc7a447f95a",
+    "url": "/static/js/1.1e780644.chunk.js"
+  },
+  {
+    "revision": "f0a941f552902f9a3acd",
+    "url": "/static/css/main.254f91b7.chunk.css"
+  },
+  {
+    "revision": "60ec99e59e060dfce46a66d16d7f88a7",
+    "url": "/index.html"
+  }
+];
