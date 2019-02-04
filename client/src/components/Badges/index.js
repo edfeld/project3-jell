90dbd0c297ac73010d9a0d5ccb25fb0fe2b7899b
Logan Crecraft <logandalec@gmail.com>
2019-02-03 22:22:23 -0800

    badges

--- client/src/components/Badges/index.js
+++ client/src/components/Badges/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import "./style.css";
-import icons from '../../../public/badges.json';
+import icons from './badges.json';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fas);
 
 // uncomment to link to user page.
 //import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 
-const Badges = props => {
-    const badges = props.data.badges.split(":");
-
-    return(
-        <div className="badges">
-            {badges.map(badge => {
-                <i className={icons[{badge}]}></i>
-            })}
-        </div>
-    )
+const Badge = props => {
+    const data = props.badge;
+    const badge = icons[data];
+    return (
+        <FontAwesomeIcon icon={badge}/>
+    );
 
 };
 
-export default Badges;
+export default Badge;
--- client/src/pages/FullPost/PostComments.js
+++ client/src/pages/FullPost/PostComments.js
@@ -1,14 +1,19 @@
 import React from 'react';
-
+import Badge from '../../components/Badges'
 
 const PostComments = props =>{
-   const key = props.data.id
+   const key = props.data.id;
+   const badges = props.data.user.badges.split(':');
+   console.log("badges", badges);
    return (
    <div key={key}>
         <header className="card" style={{display: 'inline-flex', width: '75%', margin: '1rem'}}>
         <div className="card-body">
             <h6>Username:</h6>
             <h2 className="card-title">{props.data.user.username}</h2>
+            {badges.map(badge => (
+                <Badge badge={badge}></Badge>
+            ))}
             <h6>Response:</h6>
             <h4 className="card-text">{props.data.content}</h4>
             <p><button onClick={() => props.upvote(key)} style={{background: 'none',border: 'none'}}>+</button>Up Votes: {props.data.upVotes}/<button onClick={() => props.downvote(key)} style={{background: 'none', border: 'none'}}>-</button>Down Votes: {props.data.downVotes}</p>
